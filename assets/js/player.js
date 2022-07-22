eff975034a8888551cf21b1750ddfcc522fc9e9a
hippomu-media <74494691+hippomu-media@users.noreply.github.com>
2022-07-22 19:00:50 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -390,6 +390,9 @@ window.addEventListener("message", async e => {
 
 let titlea = video_config_media['metadata']['title'];
 document.getElementById("title").innerHTML = titlea;
+let subpre = buildTracks(tracks);
+document.getElementById("subpre").innerHTML = subpre;
+
 let episodenum = video_config_media['metadata']['display_episode_number'];
 let epi = "EPISODE ";
 document.getElementById("episode").innerHTML = epi + episodenum;
